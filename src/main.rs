1d01c05bd551e55da0b324f789753e66d206da63
Khai Bui <iahknaux@github.com>
2026-03-27 12:42:07 +0700

    Add main.rs with plugin integration and game states

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use bevy::prelude::*;
+
+mod plugins {
+    pub mod map_plugin;
+    pub mod player_plugin;
+    pub mod physics_plugin;
+    pub mod card_plugin;
+    pub mod ui_plugin;
+}
+
+use plugins::{
+    map_plugin::MapPlugin,
+    player_plugin::PlayerPlugin,
+    physics_plugin::PhysicsPlugin,
+    card_plugin::CardPlugin,
+    ui_plugin::UIPlugin,
+};
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum GameState {
+    Menu,
+    CardSelection,
+    PlayerTurn,
+    ProjectileFlying,
+    RoundEnd,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .add_state(GameState::Menu)
+        .add_plugin(MapPlugin)
+        .add_plugin(PlayerPlugin)
+        .add_plugin(PhysicsPlugin)
+        .add_plugin(CardPlugin)
+        .add_plugin(UIPlugin)
+        .run();
 }
