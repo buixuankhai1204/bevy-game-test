188b3d41c6214c5e8c398e4dd37e81405da6fdf7
Khai Bui <iahknaux@github.com>
2026-03-27 12:56:47 +0700

    Update CardUI with detailed card types and selection logic

--- src/plugins/card_plugin/ui.rs
+++ src/plugins/card_plugin/ui.rs
@@ -1,11 +1,12 @@
 use bevy::prelude::*;
+use crate::plugins::card_plugin::card::{Card, CardManager, CardType};
 
 pub struct CardUIPlugin;
 
 impl Plugin for CardUIPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup_card_ui)
-           .add_system(handle_card_clicks);
+           .add_system(handle_card_interactions);
     }
 }
 
@@ -28,30 +29,32 @@ fn setup_card_ui(mut commands: Commands, asset_server: Res<AssetServer>, mut mat
                     format!("Card {}", i + 1),
                     TextStyle {
                         font: font.clone(),
-                        font_size: 40.0,
+                        font_size: 20.0,
                         color: Color::WHITE,
                     },
                     Default::default(),
                 ),
                 ..Default::default()
             });
-        });
+        }).insert(Card { card_type: match i {
+            0 => CardType::Power(1.5),
+            1 => CardType::Luck { multiplier: 2.0, count: 3 },
+            _ => CardType::WeatherControl(Vec2::new(0.0, 1.0)),
+        } });
     }
 }
 
-fn handle_card_clicks(mut interaction_query: Query<(&Interaction, &mut Style), (Changed<Interaction>, With<Button>)>) {
-    for (interaction, mut style) in interaction_query.iter_mut() {
+fn handle_card_interactions(
+    mut interaction_query: Query<(&Interaction, &Card, Entity), (Changed<Interaction>, With<Button>)>,
+    mut card_manager: ResMut<CardManager>,
+) {
+    for (interaction, card, entity) in interaction_query.iter_mut() {
         match *interaction {
             Interaction::Clicked => {
-                style.size = Size::new(Val::Px(160.0), Val::Px(75.0));
-                info!("Card Clicked");
+                card_manager.add_card(card.clone());
+                info!("Card added to selection: {:?}", card.card_type);
             }
-            Interaction::Hovered => {
-                style.size = Size::new(Val::Px(155.0), Val::Px(70.0));
-            }
-            Interaction::None => {
-                style.size = Size::new(Val::Px(150.0), Val::Px(65.0));
-            }
+            _ => {}
         }
     }
 }
